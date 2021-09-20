8fc3fca3fcc6d2acccb68d695ef83a9724a21b7c
Matthew Sievers <mattersievers@gmail.com>
2021-09-20 03:15:41 -0500

    id route and add category route function

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { RAW_KEY_NAME } = require('sequelize/lib/instance-validator');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -21,11 +22,32 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    include: {
+      model: Product,
+      attributes: ['product_name']
+    },
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
