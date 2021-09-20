044d298501f6e354f783fb133043faec6289a42d
Matthew Sievers <mattersievers@gmail.com>
2021-09-20 02:01:49 -0500

    added a few properties to models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,16 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
